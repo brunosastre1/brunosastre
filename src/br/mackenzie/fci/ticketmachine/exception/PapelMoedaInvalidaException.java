66cf0e0041944eab366ab9eac851a484798149b8
brunosastre1 <bruno.sastre@icloud.com>
2015-04-02 11:21:57 -0300

    Adicionar Header 1

--- src/br/mackenzie/fci/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ src/br/mackenzie/fci/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -1,12 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Bruno Sastre - 41316861
  */
 package br.mackenzie.fci.ticketmachine.exception;
 
-/**
- *
- * @author 1146132
- */
 public class PapelMoedaInvalidaException extends Exception {
 }
